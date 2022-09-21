45cefecdeb2d133f45d27e8066edb892257c14da
Jon Toy <jonathon.toy@gmail.com>
2022-09-20 23:16:05 -0700

    Modal Beginnings

--- src/components/FGCTimelineItem.js
+++ src/components/FGCTimelineItem.js
@@ -11,8 +11,34 @@ import PublicOutlinedIcon from "@mui/icons-material/PublicOutlined";
 import SportsEsportsRoundedIcon from "@mui/icons-material/SportsEsportsRounded";
 import LocationCityOutlinedIcon from "@mui/icons-material/LocationCityOutlined";
 import PersonIcon from "@mui/icons-material/Person";
+import Modal from "@mui/material/Modal";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "#cdd1c4",
+  borderTop: "10px solid #30323d",
+  borderLeft: "10px solid #30323d",
+  borderRight: "10px solid #30323d",
+  borderBottom: "10px solid #30323d",
+  boxShadow: 24,
+  p: 4,
+  padding: "none",
+};
 
 export default function FGCTimelineItem({ data }) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   let icon = <AccountCircleRoundedIcon />;
   let variant = "outlined";
   let timelineDotColor = "primary";
@@ -40,29 +66,56 @@ export default function FGCTimelineItem({ data }) {
   if (!data.relevantGuests) data.relevantGuests = "";
 
   return (
-    <TimelineItem>
-      <TimelineOppositeContent
-        sx={{ m: "auto 0" }}
-        variant="body2"
-        color="text.secondary"
+    <div>
+      <TimelineItem onClick={handleOpen}>
+        <TimelineOppositeContent
+          sx={{ m: "auto 0" }}
+          variant="body2"
+          color="text.secondary"
+        >
+          {data.eventType}
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+          <TimelineDot color={timelineDotColor} variant={variant}>
+            {icon}
+          </TimelineDot>
+          <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: "24px", px: 2 }}>
+          <Typography component="span" variant="h6">
+            {data.event}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {data.relevantGuests}
+          </Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
       >
-        {data.eventType}
-      </TimelineOppositeContent>
-      <TimelineSeparator>
-        <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
-        <TimelineDot color={timelineDotColor} variant={variant}>
-          {icon}
-        </TimelineDot>
-        <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
-      </TimelineSeparator>
-      <TimelineContent sx={{ py: "24px", px: 2 }}>
-        <Typography component="span" variant="h6">
-          {data.event}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {data.relevantGuests}
-        </Typography>
-      </TimelineContent>
-    </TimelineItem>
+        <Card sx={modalStyle}>
+          <CardMedia component="img" height="140" image={data.imageUrl} />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {data.event}
+            </Typography>
+            <Typography variant="body" color="text.secondary" component="div">
+              {data.longDescription}
+            </Typography>
+            <Typography variant="body2" color="text.secondary" component="div">
+              Relevant Guests: {data.relevantGuests}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Modal>
+    </div>
   );
 }
